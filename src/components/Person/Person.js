b73d58efb6af7b56d54f60f583ca3c7424446de0
Sarah <sarahrudy@gmail.com>
2021-09-23 18:36:20 -0600

    Add contents to Person component

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import './Person.css';
 
-// PERSON COMPONENT CODE GOES HERE
+const Person = props => {
+  return (
+    <section>
+      <img src={props.photo} alt='person'/>
+      <h2>{props.name}</h2>
+      <p>{props.quote}</p>
+      <p>{props.superlative}</p>
+    </section>
+  )
+}
 
 export default Person;
