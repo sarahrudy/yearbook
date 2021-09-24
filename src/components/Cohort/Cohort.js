15fe6a0f353dd68fd1838ced99b753b2f69043ea
Sarah <sarahrudy@gmail.com>
2021-09-23 18:36:39 -0600

    Add content to Cohort component

--- src/components/Cohort/Cohort.js
+++ src/components/Cohort/Cohort.js
@@ -2,6 +2,25 @@ import React from 'react';
 import Person from '../Person/Person';
 import './Cohort.css';
 
-// COHORT COMPONENT CODE GOES HERE
+const Cohort = ({staff}) => {
+  const allStaff = staff.map(staffMember => { 
+    return <Person 
+        key={staffMember.id} 
+        id={staffMember.id} 
+        name={staffMember.name} 
+        quote={staffMember.quote} 
+        superlative={staffMember.superlative} 
+        photo={staffMember.photo} 
+      />
+
+  })
+  return (
+    <div className="cohort-card">
+      <h2>what's up party people</h2>
+      <div>{allStaffMember}</div>
+    </div>
+  )
+}
+
 
 export default Cohort;
